febff07bb9ac47d8d76613e71fc4126dce6eabc4
Garv2022 <garv7183@gmail.com>
2024-06-01 13:13:47 +0530

    Creating different news categories.
    Modifying app for better user experience, different news categories are curated.

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Navbar from './Components/Navbar';
 import News from './Components/News';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar/>
-        <News pageSize={6} />
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" key="general" element={<News pageSize={6} country="in" category="general" />} />
+            <Route exact path="/business" key="business" element={<News pageSize={6} country="in" category="business" />} />
+            <Route exact path="/entertainment" key="entertainment" element={<News pageSize={6} country="in" category="entertainment" />} />
+            <Route exact path="/general" key="general" element={<News pageSize={6} country="in" category="general" />} />
+            <Route exact path="/health" key="health" element={<News pageSize={6} country="in" category="health" />} />
+            <Route exact path="/science" key="science" element={<News pageSize={6} country="in" category="science" />} />
+            <Route exact path="/sports" key="sports" element={<News pageSize={6} country="in" category="sports" />} />
+            <Route exact path="/technology" key="technology" element={<News pageSize={6} country="in" category="technology" />} />
+          </Routes>
+        </Router>
       </div>
-    )
+    );
   }
 }
--- newsapp/src/Components/News.js
+++ newsapp/src/Components/News.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import NewsItem from './NewsItem';
 import Spinner from './Spinner';
+import PropTypes from 'prop-types';
 
 export class News extends Component {
-  constructor() {
-    super();
+  static defaultProps = {
+    country: 'in',
+    pageSize: 8,
+    category: 'general',
+  };
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  };
+
+  constructor(props) {
+    super(props);
     this.state = {
       articles: [],
       loading: false,
@@ -17,8 +30,16 @@ export class News extends Component {
     this.fetchArticles(this.state.page);
   }
 
+  async componentDidUpdate(prevProps) {
+    if (this.props.category !== prevProps.category) {
+      this.setState({ page: 1 }, () => {
+        this.fetchArticles(1);
+      });
+    }
+  }
+
   fetchArticles = async (page) => {
-    const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c468d28f7de64753a8cd4cd061de816b&page=${page}&pageSize=${this.props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c468d28f7de64753a8cd4cd061de816b&page=${page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     const data = await fetch(url);
     const parsedData = await data.json();
@@ -27,7 +48,7 @@ export class News extends Component {
       totalResults: parsedData.totalResults,
       loading: false,
     });
-  }
+  };
 
   handleNextClick = async () => {
     if (this.state.page + 1 <= Math.ceil(this.state.totalResults / this.props.pageSize)) {
@@ -37,7 +58,7 @@ export class News extends Component {
         this.fetchArticles(nextPage);
       });
     }
-  }
+  };
 
   handlePrevClick = async () => {
     if (this.state.page > 1) {
@@ -47,7 +68,7 @@ export class News extends Component {
         this.fetchArticles(prevPage);
       });
     }
-  }
+  };
 
   render() {
     const { articles, loading, page, totalResults } = this.state;
@@ -55,21 +76,21 @@ export class News extends Component {
 
     return (
       <div className="container my-3">
-        <h1 className="text-center">
+        <h1 className="text-center" style={{ margin: '40px 0px' }}>
           NewsToday: Top Headlines of the day !!
         </h1>
         {loading && <Spinner />}
         <div className="row">
           {articles && articles.map((element) => {
-            const title = element.title ? element.title.slice(0, 50) : "";
-            const description = element.description ? element.description.slice(0, 100) : "";
+            const title = element.title ? element.title.slice(0, 50) : '';
+            const description = element.description ? element.description.slice(0, 100) : '';
             return (
               <div className="col-md-4" key={element.url}>
-                <NewsItem 
-                  title={title} 
-                  description={description} 
-                  imageUrl={element.urlToImage} 
-                  newsUrl={element.url} 
+                <NewsItem
+                  title={title}
+                  description={description}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
                 />
               </div>
             );
